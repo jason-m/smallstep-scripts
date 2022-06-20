483c60ec24c3f5db6727337acbe784b0dab887ef
Jason <85453045+jason-m@users.noreply.github.com>
2022-06-20 14:20:28 -0600

    removed aws stuff and systemd

--- setup-ca.sh
+++ setup-ca.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-#
-# This script will launch and configure a step-ca SSH Certificate Authority
-# with OIDC and AWS provisioners
-#
-# See https://smallstep.com/blog/diy-single-sign-on-for-ssh/ for full instructions
 
 OIDC_CLIENT_ID="[OAuth client ID]" # from Google
 OIDC_CLIENT_SECRET="[OAuth client secret]" # from Google
@@ -14,42 +9,11 @@ EMAIL="your@email.address"
 
 OPENID_CONFIG_ENDPOINT="https://accounts.google.com/.well-known/openid-configuration"
 
-curl -sLO https://github.com/smallstep/certificates/releases/download/v0.15.4/step-certificates_0.15.4_amd64.deb
-dpkg -i step-certificates_0.15.4_amd64.deb
-
-curl -sLO https://github.com/smallstep/cli/releases/download/v0.15.2/step-cli_0.15.2_amd64.deb
-dpkg -i step-cli_0.15.2_amd64.deb
-
 # All your CA config and certificates will go into $STEPPATH.
-export STEPPATH=/etc/step-ca
+export STEPPATH=/etc/step
 mkdir -p $STEPPATH
 chmod 700 $STEPPATH
 echo $ROOT_KEY_PASSWORD > $STEPPATH/password.txt
-
-# Add a service to systemd for our CA.
-cat<<EOF > /etc/systemd/system/step-ca.service
-[Unit]
-Description=step-ca service
-After=network.target
-StartLimitIntervalSec=0
-
-[Service]
-Type=simple
-Restart=always
-RestartSec=1
-User=root
-Environment=STEPPATH=/etc/step-ca
-ExecStart=/usr/bin/step-ca ${STEPPATH}/config/ca.json --password-file=${STEPPATH}/password.txt
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-LOCAL_HOSTNAME=`curl -s http://169.254.169.254/latest/meta-data/local-hostname`
-LOCAL_IP=`curl -s http://169.254.169.254/latest/meta-data/local-ipv4`
-PUBLIC_HOSTNAME=`curl -s http://169.254.169.254/latest/meta-data/public-hostname`
-PUBLIC_IP=`curl -s http://169.254.169.254/latest/meta-data/public-ipv4`
-AWS_ACCOUNT_ID=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep accountId | awk '{print $3}' | sed  's/"//g' | sed 's/,//g'`
 
 # Set up our basic CA configuration and generate root keys
 step ca init --ssh --name="$CA_NAME" \
@@ -64,9 +28,6 @@ step ca provisioner add Google --type=oidc --ssh \
     --configuration-endpoint="$OPENID_CONFIG_ENDPOINT" \
     --domain="$ALLOWED_DOMAIN"
 
-# Add the AWS provisioner, for host bootstrapping
-step ca provisioner add "Amazon Web Services" --type=AWS --ssh \
-    --aws-account=$AWS_ACCOUNT_ID
 
 # The sshpop provisioner lets hosts renew their ssh certificates
 step ca provisioner add SSHPOP --type=sshpop --ssh
